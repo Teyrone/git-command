086c24af1660d949111b8315090eb42142a23836
TeyroneKorol <teyrone.korol.vidal@gmail.com>
2025-04-21 23:58:29 -0300

    Adiciona teste na feature

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express')
 const port = process.env.PORT || 3001
 const app = express()
 
+app.get('/', (req,res) =>{
+
+    console.log('New Feature')
+})
+
 app.listen (port, ()=> {
 
     console.log( `Listen está on PAPAI!!!! na porta: ${port}`)
